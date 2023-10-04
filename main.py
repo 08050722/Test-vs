7b5ad0db30d3215f3803d76b1fecf334c9e78ea5
Cordova <icordova@cps.k12.il.us>
2023-10-04 08:55:35 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# data abstruction - when you need to simtify data 
+# from something more complex
+# Example:
+# list1 = [name1, name2, name3]
+
+
+
+#an example of data absraction is when I use a list to simplify data
+#instead of having to write out the same code over and over again
+#data abstraction is when you simplify data
+# if you have a list of names, you can simplify it by using a list
+
+
+
